5cf89234428a1b91739a144448d15dd11f465ed4
zuiidea <zuiiidea@gmail.com>
2016-11-23 23:51:09 +0800

    number card

--- src/components/dashboard/numberCard.js
+++ src/components/dashboard/numberCard.js
@@ -6,10 +6,10 @@ function NumberCard(props) {
   const {icon, color, title, number} = props
   return (
     <div className={styles.numberCard}>
-      <Icon className={styles.iconWarp} type={icon}/>
+      <Icon className={styles.iconWarp} style={{color}} type={icon}/>
       <div className={styles.content}>
-        <p className={styles.title}>{title||'Online Review'}</p>
-        <p className={styles.number}>{number||'1654,563'}</p>
+        <p className={styles.title}>{title||'No Title'}</p>
+        <p className={styles.number}>{number||'10,000'}</p>
       </div>
     </div>
   )
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,31 +1,55 @@
-import React, { PropTypes } from 'react'
-import { Link } from 'dva/router'
-import { Row, Col, Icon } from 'antd'
+import React, {PropTypes} from 'react'
+import {Link} from 'dva/router'
+import {Row, Col, Icon} from 'antd'
 import NumberCard from '../components/dashboard/numberCard'
 
+const [colorGreen,
+  colorBlue,
+  colorPurple,
+  colorRed] = ['#64ea91', '#8fc9fb', '#d897eb', '#f69899']
+
+const numberCardData = [
+  {
+    icon: 'dot-chart',
+    color: colorGreen,
+    title:'Online Review',
+    number:'2,781'
+  },
+  {
+    icon: 'dot-chart',
+    color: colorBlue,
+    title:'New Customers',
+    number:'3,241'
+  },
+  {
+    icon: 'dot-chart',
+    color: colorPurple,
+    title:'Active Projects',
+    number:'253'
+  },
+  {
+    icon: 'dot-chart',
+    color: colorRed,
+    title:'Referrals',
+    number:'4,324'
+  }
+]
+
 function Dashboard() {
+  const numberCards = numberCardData.map((item,key) => <Col  key={key} lg={6} md={12}>
+    <NumberCard {...item}/>
+  </Col>)
   return (
     <div>
-      <Row type="flex" justify="space-between">
-       <Col span={5}>
-         <NumberCard icon='dot-chart' />
-       </Col>
-       <Col span={5}>
-         <NumberCard icon='dot-chart' />
-       </Col>
-       <Col span={5}>
-         <NumberCard icon='dot-chart' />
-       </Col>
-       <Col span={5}>
-         <NumberCard icon='dot-chart' />
-       </Col>
-     </Row>
+      <Row gutter={24}>
+        {numberCards}
+      </Row>
     </div>
   )
 }
 
 Dashboard.propTypes = {
-  location: PropTypes.object,
+  location: PropTypes.object
 }
 
 export default Dashboard
