6a54b616ec427f66db3b7a48a8d0c6e61c0388b2
zuiidea <zuiiidea@gmail.com>
2016-11-28 23:31:15 +0800

    dashbord layout

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,6 +3,7 @@ import {Link} from 'dva/router'
 import {Row, Col, Icon, Card} from 'antd'
 import NumberCard from '../components/dashboard/numberCard'
 import Sales from '../components/dashboard/sales'
+import styles from './dashboard.less'
 import {color} from '../utils'
 
 const numberCardData = [
@@ -36,21 +37,43 @@ function Dashboard() {
   const numberCards = numberCardData.map((item,key) => <Col  key={key} lg={6} md={12}>
     <NumberCard {...item}/>
   </Col>)
-  return (
-    <div>
-      <Row gutter={24}>
-        {numberCards}
-      </Row>
-      <Row gutter={24}>
-        <Col lg={18} md={24}>
-          <Card bordered={false} bodyStyle={{padding:'24px 36px 24px 0'}}>
-           <Sales />
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  )
-}
+  return (<Row gutter={24}>
+          {numberCards}
+          <Col lg={18} md={24}>
+            <Card bordered={false} bodyStyle={{padding:'24px 36px 24px 0'}}>
+             <Sales />
+            </Card>
+          </Col>
+          <Col lg={6} md={24}>
+            <Row gutter={24}>
+             <Col lg={24} md={12}>
+               <Card bordered={false} bodyStyle={{padding:0,height:204,background:color.blue}}></Card>
+             </Col>
+             <Col lg={24} md={12}>
+               <Card bordered={false} bodyStyle={{padding:0,height:204,background:color.purple}}></Card>
+             </Col>
+           </Row>
+          </Col>
+          <Col lg={12} md={24}>
+            <Card bordered={false} bodyStyle={{padding:0,height:432,background:color.red}}></Card>
+          </Col>
+          <Col lg={12} md={24}>
+            <Card bordered={false} bodyStyle={{padding:0,height:432,background:color.green}}></Card>
+          </Col>
+          <Col lg={24} md={24}>
+            <Card bordered={false} bodyStyle={{padding:0,height:432,background:'#fff'}}></Card>
+          </Col>
+          <Col lg={8} md={24}>
+            <Card bordered={false} bodyStyle={{padding:0,height:432,background:'#fff'}}></Card>
+          </Col>
+          <Col lg={8} md={24}>
+            <Card bordered={false} bodyStyle={{padding:0,height:432,background:'#fff'}}></Card>
+          </Col>
+          <Col lg={8} md={24}>
+            <Card bordered={false} bodyStyle={{padding:0,height:432,background:'#fff'}}></Card>
+          </Col>
+        </Row>
+      )}
 
 Dashboard.propTypes = {
   location: PropTypes.object
