b781d9d4c8ce31daf0893a813ad02f0b44379317
zuiidea <zuiiidea@gmail.com>
2016-11-28 23:59:53 +0800

    switchSIder

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,18 @@
 import React, { PropTypes } from 'react'
-import { Menu, Icon } from 'antd'
+import { Menu, Icon, Button } from 'antd'
 import { Link } from 'dva/router'
 import styles from './main.less'
 
 const SubMenu = Menu.SubMenu
 
 function Header({ location, user, logout }) {
-  function handleClickMenu(item) {
-    if (item.key === 'logout') {
-      logout()
-    }
-  }
+  let handleClickMenu = e => e.key === 'logout' && logout()
+  let switchSider = () => {}
   return (
     <div className={styles.header}>
+      <div className={styles.siderbutton} onClick={switchSider}>
+        <Icon type="menu-fold" />
+      </div>
       <Menu className="header-menu" mode="horizontal" onClick={handleClickMenu}>
         <SubMenu style={{ float: 'right' }}
           title={<span><Icon type="user" />{user.name}</span>}
