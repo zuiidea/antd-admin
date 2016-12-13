48894e63aa94be94796a90e2609b9c90c75297f3
zuiidea <zuiiidea@gmail.com>
2016-12-13 23:34:04 +0800

    prefix

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: 'Ant Design Admin',
+  prefix:'antdAdmin',
   footerText: 'Ant Design Admin 版权所有 © 2016 由 zuiidea 支持',
   logoSrc:'https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg',
   logoText:'Antd Admin',
--- src/utils/mockStorge.js
+++ src/utils/mockStorge.js
@@ -1,8 +1,9 @@
 const Mock = require('mockjs')
 const Watch = require("watchjs")
+import {config} from './index'
 
 export default function mockStorge(name, defaultValue) {
-  let key = 'antdAdmin' + name
+  let key = config.prefix + name
   global[key] = localStorage.getItem(key)
     ? JSON.parse(localStorage.getItem(key))
     : defaultValue
