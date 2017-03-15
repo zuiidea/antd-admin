a6f2a2a67a1c171c3734a7d271872abf1e3795fb
zuiidea <zuiiidea@gmail.com>
2017-03-15 16:31:43 +0800

    add: ui-component layer

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,6 +1,7 @@
 import Ico from './ico.js'
 import Search from './search.js'
 import DropOption from './dropOption.js'
-import Layer from './layer.js'
 
-export { Ico, Search, DropOption, Layer }
+import layer from './layer.js'
+
+export { Ico, Search, DropOption, layer }
