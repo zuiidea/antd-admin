f7616d524ba5ff9e1bc6f532e9b1cc34630c8272
zuiidea <zuiiidea@gmail.com>
2017-04-01 17:53:06 +0800

    update request

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,10 +4,8 @@ module.exports = {
   footerText: 'Ant Design Admin 版权所有 © 2016 由 zuiidea 支持',
   logoSrc: 'https://t.alipayobjects.com/images/T1QUBfXo4fXXXXXXXX.png',
   logoText: 'Antd Admin',
-  needLogin: true,
   iconFontUrl: '//at.alicdn.com/t/font_c4y7asse3q1cq5mi.js',
   baseURL: 'http://47.92.30.98:7001/api',
-  crossDomains: [
-    'http://www.zuimeitianqi.com',
-  ],
+  YQL: ['http://www.zuimeitianqi.com'],
+  CORS: ['http://localhost:7001'],
 }
