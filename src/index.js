cfd0172204f2358263cdd85320759987f0937c3f
zuiidea <zuiiidea@gmail.com>
2017-04-19 19:10:20 +0800

    error message

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import 'babel-polyfill'
 import dva from 'dva'
 import createLoading from 'dva-loading'
 import { browserHistory } from 'dva/router'
+import { message } from 'antd'
 
 // 1. Initialize
 const app = dva({
   ...createLoading(),
   history: browserHistory,
   onError (error) {
-    console.error('app onError -- ', error)
+    message.error(error.message)
   },
 })
 
