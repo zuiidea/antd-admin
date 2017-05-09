bb04de647851f0928dd50180df569bd32bbfb2f9
zhuzheng <43430109@qq.com>
2017-05-09 15:55:41 +0800

    使用 antd 原生 Layout 控件替换自定义  Header, Content, Footer, Sider 等控件

--- src/components/Layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './Footer.less'
-import { config } from '../../utils'
-
-const Footer = () => <div className={styles.footer}>
-  {config.footerText}
-</div>
-
-export default Footer
--- src/routes/app.js
+++ src/routes/app.js
@@ -2,67 +2,65 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
 import { Layout } from '../components'
-import { classnames, config, menu } from '../utils'
+import { config, menu } from '../utils'
 import { Helmet } from 'react-helmet'
 import '../themes/index.less'
 import './app.less'
 import NProgress from 'nprogress'
-const { prefix } = config
 
-const { Header, Bread, Footer, Sider, styles } = Layout
+
+const { Main } = Layout
 
 const App = ({ children, location, dispatch, app, loading }) => {
-  const { user, siderFold, darkTheme, isNavbar, menuPopoverVisible, navOpenKeys } = app
+  // const { user, siderFold, darkTheme, isNavbar, menuPopoverVisible, navOpenKeys } = app
 
   NProgress.start()
   !loading.global && NProgress.done()
 
-  const headerProps = {
-    menu,
-    user,
-    siderFold,
-    location,
-    isNavbar,
-    menuPopoverVisible,
-    navOpenKeys,
-    switchMenuPopover () {
-      dispatch({ type: 'app/switchMenuPopver' })
-    },
-    logout () {
-      dispatch({ type: 'app/logout' })
-    },
-    switchSider () {
-      dispatch({ type: 'app/switchSider' })
-    },
-    changeOpenKeys (openKeys) {
-      dispatch({ type: 'app/handleNavOpenKeys', payload: { navOpenKeys: openKeys } })
-    },
-  }
+  // const headerProps = {
+  //   menu,
+  //   user,
+  //   siderFold,
+  //   location,
+  //   isNavbar,
+  //   menuPopoverVisible,
+  //   navOpenKeys,
+  //   switchMenuPopover() {
+  //     dispatch({ type: 'app/switchMenuPopver' })
+  //   },
+  //   logout() {
+  //     dispatch({ type: 'app/logout' })
+  //   },
+  //   switchSider() {
+  //     dispatch({ type: 'app/switchSider' })
+  //   },
+  //   changeOpenKeys(openKeys) {
+  //     dispatch({ type: 'app/handleNavOpenKeys', payload: { navOpenKeys: openKeys } })
+  //   },
+  // }
 
-  const siderProps = {
-    menu,
-    siderFold,
-    darkTheme,
-    location,
-    navOpenKeys,
-    changeTheme () {
-      dispatch({ type: 'app/switchTheme' })
-    },
-    changeOpenKeys (openKeys) {
-      localStorage.setItem(`${prefix}navOpenKeys`, JSON.stringify(openKeys))
-      dispatch({ type: 'app/handleNavOpenKeys', payload: { navOpenKeys: openKeys } })
-    },
-  }
+  // const siderProps = {
+  //   menu,
+  //   siderFold,
+  //   darkTheme,
+  //   location,
+  //   navOpenKeys,
+  //   changeTheme() {
+  //     dispatch({ type: 'app/switchTheme' })
+  //   },
+  //   changeOpenKeys(openKeys) {
+  //     localStorage.setItem(`${prefix}navOpenKeys`, JSON.stringify(openKeys))
+  //     dispatch({ type: 'app/handleNavOpenKeys', payload: { navOpenKeys: openKeys } })
+  //   },
+  // }
 
-  const breadProps = {
-    menu,
-  }
+  // const breadProps = {
+  //   menu,
+  // }
 
   if (config.openPages && config.openPages.indexOf(location.pathname) > -1) {
     return <div>{children}</div>
   }
-
-  const { iconFontJS, iconFontCSS } = config
 
   return (
     <div>
@@ -70,24 +68,9 @@ const App = ({ children, location, dispatch, app, loading }) => {
         <title>ANTD ADMIN</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href={config.logoSrc} type="image/x-icon" />
-        {iconFontJS && <script src={iconFontJS}></script>}
-        {iconFontCSS && <link rel="stylesheet" href={iconFontCSS} />}
+        {config.iconFontUrl ? <script src={config.iconFontUrl}></script> : ''}
       </Helmet>
-      <div className={classnames(styles.layout, { [styles.fold]: isNavbar ? false : siderFold }, { [styles.withnavbar]: isNavbar })}>
-        {!isNavbar ? <aside className={classnames(styles.sider, { [styles.light]: !darkTheme })}>
-          <Sider {...siderProps} />
-        </aside> : ''}
-        <div className={styles.main}>
-          <Header {...headerProps} />
-          <Bread {...breadProps} location={location} />
-          <div className={styles.container}>
-            <div className={styles.content}>
-              {children}
-            </div>
-          </div>
-          <Footer />
-        </div>
-      </div>
+      <Main {...{ children, location, dispatch, app, menu }} />
     </div>
   )
 }
