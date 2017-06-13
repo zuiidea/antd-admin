04b147d33fb2527a6a363a433a2eda0d27a32c8c
zuiidea <zuiiidea@gmail.com>
2017-06-13 20:30:24 +0800

    proxy example

--- .roadhogrc.js
+++ .roadhogrc.js
@@ -9,6 +9,19 @@ export default {
   entry: 'src/index.js',
   svgSpriteLoaderDirs: svgSpriteDirs,
   "theme": "./theme.config.js",
+  // 接口代理示例
+  // "proxy": {
+  //   "/api/v1": {
+  //     "target": "http://api.zuiidea.com",
+  //     "changeOrigin": true,
+  //     "pathRewrite": { "^/api/v1" : "/v1" }
+  //   },
+  //   "/api/v2": {
+  //     "target": "http://192.168.0.110",
+  //     "changeOrigin": true,
+  //     "pathRewrite": { "^/api/v2" : "/api/v2" }
+  //   }
+  // },
   "env": {
       "development": {
         "extraBabelPlugins": [
