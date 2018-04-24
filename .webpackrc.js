f1e2fd3b18c72e42e6ec7b1cbc5d67e6dbdee441
Baorong Li <superlbr@126.com>
2018-04-24 22:51:12 +0800

    fix: alias

--- .webpackrc.js
+++ .webpackrc.js
@@ -17,6 +17,13 @@ export default {
   },
   alias: {
     themes: resolve(__dirname, './src/themes'),
+    components: resolve(__dirname,"./src/components"),
+    utils: resolve(__dirname,"./src/utils"),
+    config: resolve(__dirname,"./src/utils/config"),
+    enums: resolve(__dirname,"./src/utils/enums"),
+    services: resolve(__dirname,"./src/services"),
+    models: resolve(__dirname,"./src/models"),
+    routes: resolve(__dirname,"./src/routes"),
   },
   urlLoaderExcludes: [
     /\.svg$/,
