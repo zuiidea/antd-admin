cd53b36e76e90f86cedcd5c45cb21c44978defc4
zuiidea <zuiiidea@gmail.com>
2018-09-08 23:39:49 +0800

    Deprecated component layer、Search、DataTable、Iconfont，because there are exist similar component in antd.

--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,15 @@
-import DataTable from './DataTable'
-import DropOption from './DropOption'
-import Search from './Search'
 import Editor from './Editor'
 import FilterItem from './FilterItem'
+import DropOption from './DropOption'
 import Loader from './Loader'
 import * as MyLayout from './Layout/index.js'
 import Page from './Page'
 
-import layer from './layer'
-
 export {
   MyLayout,
-  DataTable,
-  DropOption,
-  Search,
   Editor,
   FilterItem,
+  DropOption,
   Loader,
-  layer,
   Page,
 }
--- src/pages/UIElement/iconfont/emoji.js
+++
@@ -1,8 +0,0 @@
-import '../../../svg/emoji/vomiting.svg'
-import '../../../svg/emoji/smirking.svg'
-import '../../../svg/emoji/surprised.svg'
-import '../../../svg/emoji/unamused.svg'
-import '../../../svg/emoji/zombie.svg'
-import '../../../svg/emoji/tired.svg'
-import '../../../svg/emoji/tongue.svg'
-import '../../../svg/emoji/wink.svg'
