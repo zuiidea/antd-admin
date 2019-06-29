838dc93eff8179b23d79c5f9b59b5f639ed4a4bc
Baorong Li <superlbr@gmail.com>
2019-06-29 10:56:03 +0800

    bump antd-admin@5.1.1

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteName: 'AntD Admin',
-  copyright: 'Ant Design Admin  © 2018 zuiidea',
+  copyright: 'Ant Design Admin  ©2019 zuiidea',
   logoPath: '/logo.svg',
   apiPrefix: '/api/v1',
   fixedHeader: true, // sticky primary layout header
